1a69c1f65bcbf301123e5807cb4446877c9d7bcf
MAbdullah-dev <mabdullahwebdev@gmail.com>
2024-09-30 17:46:29 -0400

    frontend updates

--- views/courses.php
+++ views/courses.php
@@ -1,6 +1,6 @@
-<?php 
-require_once "./components/header.php"; 
-require_once "../controllers/makeCourseController.php"; 
+<?php
+require_once "./components/header.php";
+require_once "../controllers/makeCourseController.php";
 
 $controller = new MakeCourseController();
 $courses = $controller->getCourses(); // Fetch all courses
@@ -16,12 +16,15 @@ $courses = $controller->getCourses(); // Fetch all courses
                         <h5 class="card-title text-primary"><?php echo htmlspecialchars($course['title']); ?></h5>
                         <p class="card-text"><?php echo htmlspecialchars($course['description']); ?></p>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Created by: <?php echo htmlspecialchars($course['creator_name']); ?></li>
-                            <li class="list-group-item">Price: <?php echo ($course['price'] > 0) ? '$' . htmlspecialchars($course['price']) : 'Free'; ?></li>
+                            <div class="d-flex justify-content-between align-items-center border-bottom p-2">
+                                <li class="list-unstyled ">Price: <?php echo ($course['price'] > 0) ? '$' . htmlspecialchars($course['price']) : 'Free'; ?></li>
+                                <li class="list-unstyled text-capitalize">Course Level: <?php echo htmlspecialchars($course['type']); ?></li>
+                            </div>
+                            <li class="list-unstyled mt-3 text-capitalize">Created by: <?php echo htmlspecialchars($course['creator_name']); ?></li>
                         </ul>
                     </div>
                     <div class="card-footer">
-                        <a href="./view_course.php?id=<?php echo $course['id']; ?>" class="btn btn-primary w-100">View Course</a>
+                        <a href="./view_classes.php?id=<?php echo $course['id']; ?>" class="btn btn-primary w-100">View Course</a>
                     </div>
                 </div>
             </div>
