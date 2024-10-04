080010b013d12eb8659ab14c48c3f00c4671f782
m-ahmed321 <m.ahmed.diligent@gmail.com>
2024-10-05 00:47:32 +0500

    footer reworked

--- views/components/footer.php
+++ views/components/footer.php
@@ -1,11 +1,4 @@
-<footer class="footer">
-    <span>
-        Copyright &copy; Company Name
-        </label>
-        <ul>
-            <li class="nav-item"><a href="#">Privacy Policy</a></li>
-            <li class="nav-item"><a href="#">Terms of use</a></li>
-        </ul>
+<footer class="">
 </footer>
 <script src="../public/bootstrap/js/bootstrap.min.js"></script>
 <script src="../public/assets/js/script.js"></script>
